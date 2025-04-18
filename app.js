e5323979585a04f90cf266e42bc15e419dcec867
Udaykumar4058 <uk850485@gmail.com>
2025-04-19 00:18:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+let cbtn=document.getElementById("cbtn");
+let cnt=document.querySelector(".container");
+let add=document.querySelector(".add-list-conatainer");
+let listName=document.querySelector("#NameOfTrack");
+let naam=document.getElementById("list-name");
+let addbtn=document.getElementById("addbtn");
+let
+
+// naam.innerText="listName.value";
+
+cbtn.addEventListener("click",()=>{
+  cnt.style.display = "none";
+  add.style.display="flex";
+  naam.innerText=listName.value;
+});
+
+
+
+// console.log();
